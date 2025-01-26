74b9f4164353826e006a96ad5f9b8b0ea8936805
Kat <superkat1806@gmail.com>
2025-01-25 22:19:41 -0500

    Water body merging

--- src/main/java/net/superkat/tidal/TidalWaveHandler.java
+++ src/main/java/net/superkat/tidal/TidalWaveHandler.java
@@ -1,6 +1,7 @@
 package net.superkat.tidal;
 
 import com.google.common.collect.Lists;
+import com.google.common.collect.Maps;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.Blocks;
 import net.minecraft.client.MinecraftClient;
@@ -20,7 +21,9 @@ import org.apache.commons.compress.utils.Sets;
 import org.jetbrains.annotations.Nullable;
 
 import java.util.List;
+import java.util.Map;
 import java.util.Set;
+import java.util.stream.Collectors;
 
 public class TidalWaveHandler {
 
@@ -43,7 +46,7 @@ public class TidalWaveHandler {
 
         if(player.getActiveItem().isOf(Items.SPYGLASS) && player.getItemUseTime() >= 10) {
             for (int i = 0; i < 10; i++) {
-                tickRandomWaterBody(player);
+                tick(player);
             }
         }
 
@@ -68,6 +71,8 @@ public class TidalWaveHandler {
                 ParticleTypes.ELECTRIC_SPARK
         );
 
+        if(world.getTime() % 10 != 0) return;
+
         int i = 0;
         for (WaterBody water : waterBodies) {
             i++;
@@ -77,12 +82,12 @@ public class TidalWaveHandler {
             ParticleEffect particle = particles.get(i);
             water.waterBlocks.forEach(blockPos -> {
                 Vec3d pos = blockPos.toCenterPos();
-                this.world.addParticle(particle, pos.getX(), pos.getY() + 1, pos.getZ(), 0, 0.3, 0);
+                this.world.addParticle(particle, pos.getX(), pos.getY() + 1, pos.getZ(), 0, 0, 0);
             });
         }
     }
 
-    public void tickRandomWaterBody(ClientPlayerEntity player) {
+    public void tick(ClientPlayerEntity player) {
         BlockPos playerPos = player.getBlockPos();
         BlockPos.Mutable pos = new BlockPos.Mutable();
         int horizontalRadius = TidalConfig.horizontalWaveDistance;
@@ -96,38 +101,75 @@ public class TidalWaveHandler {
         int z = playerPos.getZ() + random.nextInt(horizontalRadius) - random.nextInt(horizontalRadius);
         pos.set(x, y, z);
 
-        checkPosWaterBody(pos);
+        tickPos(pos);
     }
 
-    public void checkPosWaterBody(BlockPos pos) {
-        //TODO - handle blocks that have changed from water to not water
-        //TODO - increase check size for every block, and maybe increase water blocks even more to check for merging water bodies
-        if(!posIsWater(world, pos)) return;
-        if(posAlreadyInWaterBody(pos)) return;
-        WaterBody waterBody = new WaterBody(pos);
-        waterBody.addNeighbours(world, 1);
+    //I hope in time I'm able to look back at this and think, "wow, that's really inefficient",
+    //not because I want this to be inefficient, but because I hope I'll have improved and be more knowledgeable
+    public void tickPos(BlockPos pos) {
+        //3x3x3 blocks
+        Map<BlockPos, Boolean> neighbourBlocks = getBlockNeighbour(world, pos, 1, -1, 1);
+        Set<BlockPos> nonWater = neighbourBlocks.entrySet().stream()
+                .filter(entry -> !entry.getValue())
+                .map(Map.Entry::getKey)
+                .collect(Collectors.toSet());
 
+        for (BlockPos nonWaterBlock : nonWater) {
+            removeBlockFromWaterBodies(nonWaterBlock);
+            neighbourBlocks.remove(nonWaterBlock);
+        }
+
+        //neighbourBlocks are now all water blocks
+        if(neighbourBlocks.isEmpty()) return;
+        boolean initPosIsWater = neighbourBlocks.containsKey(pos);
+        BlockPos waterBodyInitPos = initPosIsWater ? pos : neighbourBlocks.keySet().stream().toList().getFirst();
+        WaterBody waterBody = new WaterBody(waterBodyInitPos);
+        waterBody.addNeighbours(world, neighbourBlocks.keySet());
         if(tryMergeWaterBody(waterBody)) return;
-        waterBodies.add(waterBody);
+
+        this.waterBodies.add(waterBody);
+    }
+
+    public static Map<BlockPos, Boolean> getBlockNeighbour(ClientWorld world, BlockPos pos, int horizontalDistance, int y1, int y2) {
+        Map<BlockPos, Boolean> cachedBlocks = Maps.newHashMap();
+
+        for (BlockPos blockPos : BlockPos.iterate(pos.add(-horizontalDistance, y1, -horizontalDistance), pos.add(horizontalDistance, y2, horizontalDistance))) {
+            cachedBlocks.computeIfAbsent(blockPos.mutableCopy(), pos1 -> posIsWater(world, pos1));
+        }
+        return cachedBlocks;
     }
 
     public boolean tryMergeWaterBody(WaterBody waterBody) {
-        WaterBody checkedWater = waterBodyAlreadyAnother(waterBody);
-        if(checkedWater == null) return false;
-        checkedWater.mergeWaterBody(waterBody);
-        waterBodies.remove(waterBody);
+        Set<WaterBody> checkedWaters = waterBodiesInAnother(waterBody);
+        if(checkedWaters.isEmpty()) return false;
+
+        WaterBody previousWater = waterBody;
+        for (WaterBody waterBody1 : checkedWaters) {
+            waterBody1.mergeWaterBody(previousWater);
+            waterBodies.remove(previousWater);
+            previousWater = waterBody1;
+        }
         return true;
     }
 
+    public Set<WaterBody> waterBodiesInAnother(WaterBody waterBody) {
+        return waterBodies.stream().filter(checkedWater -> waterBody.waterBlocks.stream().anyMatch(pos -> checkedWater.waterBlocks.contains(pos)))
+                .collect(Collectors.toSet());
+    }
+
     @Nullable
-    public WaterBody waterBodyAlreadyAnother(WaterBody waterBody) {
+    public WaterBody waterBodyInAnother(WaterBody waterBody) {
         //intellij magic oh my gosh
         return waterBodies.stream().filter(checkedWater -> waterBody.waterBlocks.stream().anyMatch(pos -> checkedWater.waterBlocks.contains(pos)))
                 .findFirst()
                 .orElse(null);
     }
 
-    public boolean posAlreadyInWaterBody(BlockPos pos) {
+    public void removeBlockFromWaterBodies(BlockPos pos) {
+        waterBodies.forEach(waterBody -> waterBody.waterBlocks.remove(pos));
+    }
+
+    public boolean posInWaterBody(BlockPos pos) {
         return waterBodies.stream().anyMatch(waterBody -> waterBody.waterBlocks.contains(pos));
     }
 
--- src/main/java/net/superkat/tidal/water/WaterBody.java
+++ src/main/java/net/superkat/tidal/water/WaterBody.java
@@ -7,6 +7,7 @@ import net.minecraft.util.math.BlockPos;
 import net.minecraft.util.math.Direction;
 
 import java.util.Set;
+import java.util.stream.Collectors;
 
 /**
  * Keep track of water blocks, and understand roughly how big areas of water are.
@@ -26,6 +27,29 @@ public class WaterBody {
 
     public void mergeWaterBody(WaterBody waterBody) {
         this.waterBlocks.addAll(waterBody.waterBlocks);
+    }
+
+    public void addBlocks(Set<BlockPos> blocks) {
+        this.waterBlocks.addAll(blocks);
+    }
+
+    public void addNeighbours(ClientWorld world, Set<BlockPos> neighbours) {
+        this.mainPos = topOfWater(world, this.initPos);
+        Set<BlockPos> topNeighbours = neighbours.stream().map(pos -> topOfWater(world, pos)).collect(Collectors.toSet());
+        addBlocks(topNeighbours);
+
+//        Map<BlockPos, Boolean> waterNeighbours = neighbours.entrySet().stream()
+//                .filter(Map.Entry::getValue)
+//                .collect(
+//                        Collectors.toMap(entry -> topOfWater(world, entry.getKey()), entry -> true)
+//                );
+
+//        Set<BlockPos> waterNeighbours = neighbours.entrySet().stream()
+//                .filter(Map.Entry::getValue) //block is water
+//                .map(entry -> topOfWater(world, entry.getKey()))
+//                .collect(Collectors.toSet());
+//
+//        this.waterBlocks.addAll(waterNeighbours);
     }
 
     public void addNeighbours(ClientWorld world, int distance) {
